98e3b2892715933c1fd7eaf0e05b1a6fa798d9fd
guru2468 <jeremie.reusser@sunrise.ch>
2015-10-17 22:54:49 +0200

    Added experimental bldc speed controlelr

---
+++ motor.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+
+COILA = 21
+COILB = 22
+COILC = 23
+
+speed = 100
+pwmSignal = [0,0,0,1,1,1]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(COILA,GPIO.OUT)
+GPIO.setup(COILB,GPIO.OUT)
+GPIO.setup(COILC,GPIO.OUT)
+
+while True:
+    if speed < 0:
+        step = -1
+
+    else:
+        step = 1
+
+    phaseA = phaseA + step
+    if phaseA > 5:
+        phaseA = 0
+    if phaseA < 0:
+        phaseA = 5
+
+    phaseB = phaseB + step
+    if phaseB > 5:
+        phaseB = 0
+    if phaseB < 0:
+        phaseB = 5
+
+    phaseC = phaseC + step
+    if phaseC > 5:
+        phaseC = 0
+    if phaseC < 0:
+        phaseC = 5
+
+    GPIO.output(COILA,pwmSignal[phaseA])
+    GPIO.output(COILb,pwmSignal[phaseB])
+    GPIO.output(COILC,pwmSignal[phaseC])
+    time.sleep(1/speed)
