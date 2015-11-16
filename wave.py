9f6d9ccad31be345a638ae383002cc77da7aba69
guru2468 <jeremie.reusser@sunrise.ch>
2015-11-16 09:17:11 +0100

    fixes

--- wave.py
+++ wave.py
@@ -7,5 +7,5 @@ y = 0
 while True:
     x = (y/360)*2*math.pi
     y += 1
-    os.system("i2cset -y 1 0x47 0x00" + hex(math.sin(x)) + " w")
+    os.system("i2cset -y 1 0x47 0x00" + hex(sin(x)) + " w")
     time.sleep(.05)
