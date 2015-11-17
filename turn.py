ea9b0ec828f1ea185863d14235b6a36cb6afccee
guru2468 <jeremie.reusser@sunrise.ch>
2015-11-17 15:25:11 +0100

    fix2

--- turn.py
+++ turn.py
@@ -6,8 +6,11 @@ x = 0
 y = 0
 while True:
     y += (2*math.pi)/360
-    os.system("i2cset -y 1 0x47 0x00" + hex(int((255*math.sin(y)+255)/2)) + " w")
-    os.system("i2cset -y 1 0x47 0x01" + hex(int((255*math.sin(y+(math.pi*(2/3))+255)/2)) + " w")
-    os.system("i2cset -y 1 0x47 0x02" + hex(int((255*math.sin(y+(math.pi*(4/3))+255)/2)) + " w")
+    os.system("i2cset -y 1 0x47 0x00" + hex(int((0.5*(255*math.sin(y)+255)))) + " w")
+    os.system("i2cset -y 1 0x47 0x01" + hex(int((0.5*(255*math.sin(y+(math.pi*(2/3)))+255)))) + " w")
+    os.system("i2cset -y 1 0x47 0x02" + hex(int((0.5*(255*math.sin(y+(math.pi*(4/3)))+255)))) + " w")
 
     time.sleep(.001)
+#(0.5*(255*math.sin(y)+255))
+#(0.5*(255*math.sin(y+(math.pi*(2/3)))+255))
+#(0.5*(255*math.sin(y+(math.pi*(4/3)))+255))
