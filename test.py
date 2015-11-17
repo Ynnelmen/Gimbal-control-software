dcff5c7b252d3e4112536d6e5e1a7d8ff4801144
guru2468 <jeremie.reusser@sunrise.ch>
2015-11-17 16:28:41 +0100

    major stuff

--- test.py
+++ test.py
@@ -2,26 +2,6 @@ import math
 import os
 import time
 
-motorposition1 = 0
-motorposition2 = 0
-motorposition3 = 0
-
-def turnX():
-    pass
-    x = 0
-    while True:
-        motorX(x)
-        x += 1
-        if x > 359:
-            x -= 0
-        time.sleep(0.001)
-
-
-def motorX(xpos):
-    os.system("i2cset -y 1 0x47 0x00 " + hex(motorposition1[xpos]) + " w")
-	os.system("i2cset -y 1 0x47 0x01 " + hex(motorposition2[xpos]) + " w")
-	os.system("i2cset -y 1 0x47 0x02 " + hex(motorposition3[xpos]) + " w")
-
 def generatesteps(resolution, offset):
     deltastep = offset
     motorstep = 0
@@ -39,4 +19,3 @@ motorposition3 = generatesteps(1,4.1887866666)
 print motorposition1
 print motorposition2
 print motorposition3
-turnX()
