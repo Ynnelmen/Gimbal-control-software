15e08c30a76070731afc7acadc4e256bdfa3f210
guru2468 <jeremie.reusser@sunrise.ch>
2015-12-21 12:08:10 +0100

    Getting ready for working gimbal
    Still need to configure correct PWM output pins. Need some more
    testing. Filtering and output control are still WIP.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # PyRoscope - IMU software for gimbal-control, written by Jeremie Reusser.
-# Beta 0.3
+# Version 1.0a
+# This software is meant to be run on Raspberry Pi B+ or Raspberry Pi 2 systems.
+# Designed for 2x ADXL345 accelerometers and 2x L3GD20/L3GD20H gyros (connected via I2C - you need to activate alternate addresses for one pair of modules).
+# Motor control through PWM through L298N h-bridge. Any BLDC should do after a bit of tweaking.
+# This is experimental technical demonstration software. For realtime performance, use embedded systems with low level implementations or dedicated controller boards.
 
 from ADXL345.adxl345 import ADXL345
 from L3GD20.L3GD20 import L3GD20
@@ -13,118 +17,120 @@ factorIN = 0.4  # bestimmt Gewichtung des neuen input-Wertes
 factor0 = 0.3   # "         "         der alten Werte
 factor1 = 0.2
 factor2 = 0.1
-DT = 0.02 # set to 0.04 for low frequency
-pwmSignal = [0,0,0,1,1,1]
-speedX = 5
-speedY = 10
+DT = 0.02 # master program duty cycle (regulator refresh rate) - set to 0.04 for low frequency
+PWM_FREQ = 200 # set master PWM refresh rate (motors only)
+
+
 motorposition1 = 0
 motorposition2 = 0
 motorposition3 = 0
 
-def motorX():
+def motorX(): # controls x-axis
     global xpos
     xpos = 0
+    x = 0
     time.sleep(30)
     while True:
-        xpos = int(output1[0])
-        if xpos > 359:
+        x = gentarget(output2[0]) # filters reference values
+        xpos = x - int(output1[0]) # calculates requested output
+        if xpos > 359: # compensate for full revolution
             xpos = xpos - 360
-        if xpos < 0:
+        if xpos < 0: # compensate for full revolution
             xpos = 360 + xpos
         time.sleep(DT/2)
-        p.ChangeDutyCycle(xmotorposition1[xpos])
-        q.ChangeDutyCycle(xmotorposition2[xpos])
-        r.ChangeDutyCycle(xmotorposition3[xpos])
+        # set phase values
+        x1.ChangeDutyCycle(xmotorposition1[xpos])
+        x2.ChangeDutyCycle(xmotorposition2[xpos])
+        x3.ChangeDutyCycle(xmotorposition3[xpos])
 
-def motorY():
+def motorY(): # controls y-axis
     global ypos
     ypos = 0
+    y = 0
     time.sleep(30)
     while True:
-        ypos = int(output1[1])
-        if ypos > 359:
+        y = gentarget(output2[1) # filters reference values
+        ypos = y - int(output1[1]) # calculates requested output
+        if ypos > 359: # compensate for full revolution
             ypos = ypos - 360
-        if ypos < 0:
+        if ypos < 0: # compensate for full revolution
             ypos = 360 + ypos
         time.sleep(DT/2)
-        s.ChangeDutyCycle(ymotorposition1[ypos])
-        t.ChangeDutyCycle(ymotorposition2[ypos])
-        u.ChangeDutyCycle(ymotorposition3[ypos])
+        # set phase values
+        y1.ChangeDutyCycle(ymotorposition1[ypos])
+        y2.ChangeDutyCycle(ymotorposition2[ypos])
+        y3.ChangeDutyCycle(ymotorposition3[ypos])
 
 def gentarget(newinput):
-	output = newinput*factorIN + prev[0]*factor0 + prev[1]*factor1 + prev[2]*factor2 ##input wird an vorherige messungen angeglichen
+	output = newinput*factorIN + prev[0]*factor0 + prev[1]*factor1 + prev[2]*factor2 #input wird an vorherige messungen angeglichen
     del prev[0]
     prev.append(output) #speichert angepassten output im array
     return (output)
 
-def generatesteps(resolution, offset):
+def generatesteps(resolution, offset): # generate array values for every possible motor microstep
     deltastep = offset
     motorstep = 0
     motormap = []
-    motormap.extend(range(1,((12*360)/resolution)+1))
-    for item in motormap:
+    motormap.extend(range(1,((12*360)/resolution)+1)) # set range as a function of requested angular resolution
+    for item in motormap: # fill list with calculated output duty cycles
         motormap[motorstep] = int((100*math.sin(deltastep)+100)/2)
         deltastep += (2*resolution*math.pi)/360
         motorstep += 1
     return motormap
 
-def filter_reference():
+def filter_reference(): # combines raw sensor data from reference pair
     global x1, y1, z1, output1
     offset = [0.0,0.0,0.0]
     output1 = [0.0,0.0,0.0]
     iterations = 0
     time.sleep(2)
     while True:
-        output1[0] = offset[0] + .99*(output1[0]+reference_gyro_omega[0]*DT-offset[0]) + .01*(360/(2*math.pi))*(math.atan2(reference_accelerometer_acc['y'], reference_accelerometer_acc['z'])+math.pi)
-        output1[1] = offset[1] + .99*(output1[1]+reference_gyro_omega[1]*DT-offset[1])+.01*(360/(2*math.pi))*(math.atan2(reference_accelerometer_acc['z'], reference_accelerometer_acc['x'])+math.pi)
-        #output1[2] = offset[2] + .99999*(output1[2]+reference_gyro_omega[2]*DT-offset[2]) + .00001*(360/(2*math.pi))*(math.atan2(reference_accelerometer_acc['x'], reference_accelerometer_acc['y'])+math.pi)
-        iterations += 1
+        output1[0] = offset[0] + .99*(output1[0]+reference_gyro_omega[0]*DT-offset[0]) + .01*(360/(2*math.pi))*(math.atan2(reference_accelerometer_acc['y'], reference_accelerometer_acc['z'])+math.pi) # calculate x-rotation
+        output1[1] = offset[1] + .99*(output1[1]+reference_gyro_omega[1]*DT-offset[1]) + .01*(360/(2*math.pi))*(math.atan2(reference_accelerometer_acc['z'], reference_accelerometer_acc['x'])+math.pi) # calculate y-rotation
+        iterations += 1 # iteration counter
         time.sleep(DT)
-        if iterations == 20*(1/DT):
-            offset[0] = 0.0 - output1[0]
-            offset[1] = 0.0 - output1[1]
-            #offset[2] = 0.0 - output1[2]
-            output1[0] = 0
-            output1[1] = 0
+        if iterations == 20*(1/DT): # set values to zero once values are stabilized based on fixed iteration number (as a function of the master clock)
+            offset[0] = 0.0 - output1[0] # zero x
+            offset[1] = 0.0 - output1[1] # zero y
+            output1[0] = 0 # output to 0 for instant response (avoid values in- or decrementing)
+            output1[1] = 0 # output to 0 for instant response (avoid values in- or decrementing)
             print "Sensor 1 calibrated"
 
-def filter_stabilized():
+def filter_stabilized(): # combines raw sensor data from stabilized pair
     global output2
     offset = [0.0,0.0,0.0]
     output2 = [0.0,0.0,0.0]
     iterations = 0
     time.sleep(2)
     while True:
-        output2[0] = offset[0] + .99*(output2[0]+stabilized_gyro_omega[0]*DT-offset[0]) + .01*(360/(2*math.pi))*(math.atan2(stabilized_accelerometer_acc['y'], stabilized_accelerometer_acc['z'])+math.pi)
-        output2[1] = offset[1] + .99*(output2[1]+stabilized_gyro_omega[1]*DT-offset[1]) + .01*(360/(2*math.pi))*(math.atan2(stabilized_accelerometer_acc['z'], stabilized_accelerometer_acc['x'])+math.pi)
-        #output2[2] = offset[2] + .99999*(output2[2]+stabilized_gyro_omega[2]*DT-offset[2]) + .00001*(360/(2*math.pi))*(math.atan2(stabilized_accelerometer_acc['x'], stabilized_accelerometer_acc['y'])+math.pi)
-        iterations += 1
+        output2[0] = offset[0] + .99*(output2[0]+stabilized_gyro_omega[0]*DT-offset[0]) + .01*(360/(2*math.pi))*(math.atan2(stabilized_accelerometer_acc['y'], stabilized_accelerometer_acc['z'])+math.pi) # calculate x-rotation
+        output2[1] = offset[1] + .99*(output2[1]+stabilized_gyro_omega[1]*DT-offset[1]) + .01*(360/(2*math.pi))*(math.atan2(stabilized_accelerometer_acc['z'], stabilized_accelerometer_acc['x'])+math.pi) # calculate y-rotation
+        iterations += 1 # iteration counter
         time.sleep(DT)
-        if iterations == 20*(1/DT):
-            offset[0] = 0.0 - output2[0]
-            offset[1] = 0.0 - output2[1]
-            output2[0] = 0
-            output2[1] = 0
-            #offset[2] = 0.0 - output2[2]
+        if iterations == 20*(1/DT): # set values to zero once values are stabilized based on fixed iteration number (as a function of the master clock)
+            offset[0] = 0.0 - output2[0] # zero x
+            offset[1] = 0.0 - output2[1] # zero y
+            output2[0] = 0 # output to 0 for instant response (avoid values in- or decrementing)
+            output2[1] = 0 # output to 0 for instant response (avoid values in- or decrementing)
             print "Sensor 2 calibrated"
 
-def accelerometer_reference():
+def accelerometer_reference(): # read reference accelerometer values via ADXL345 library
     global reference_accelerometer_acc
     reference_accelerometer_acc = 0
-    reference_accelerometer = ADXL345(0x1d)
+    reference_accelerometer = ADXL345(0x1d) # read correct I2C device address
     while True:
-        time.sleep(DT)
-        reference_accelerometer_acc = reference_accelerometer.getAxes(True)
+        time.sleep(DT) # match program clock
+        reference_accelerometer_acc = reference_accelerometer.getAxes(True) # read value
 
-def accelerometer_stabilized():
+def accelerometer_stabilized(): # read stabilized accelerometer values via ADXL345 library
     global stabilized_accelerometer_acc
     stabilized_accelerometer_acc = 0
-    stabilized_accelerometer = ADXL345(0x53)
+    stabilized_accelerometer = ADXL345(0x53) # read correct I2C device address
     while True:
-        time.sleep(DT)
-        stabilized_accelerometer_acc = stabilized_accelerometer.getAxes(True)
+        time.sleep(DT) # match program clock
+        stabilized_accelerometer_acc = stabilized_accelerometer.getAxes(True) # read value
 
-def gyro_reference():
+def gyro_reference(): # read reference gyroscope values
     global reference_gyro_omega
     reference_gyro_omega = 0
     reference_gyro = L3GD20(busId = 1, slaveAddr = 0x6a, ifLog = False, ifWriteBlock=False)
@@ -138,7 +144,8 @@ def gyro_reference():
     while True:
         time.sleep(DT)
         reference_gyro_omega = reference_gyro.Get_CalOut_Value()
-def gyro_stabilized():
+
+def gyro_stabilized(): # read stabilized gyroscope values
     global stabilized_gyro_omega
     stabilized_gyro_omega = 0
     stabilized_gyro = L3GD20(busId = 1, slaveAddr = 0x6b, ifLog = False, ifWriteBlock=False)
@@ -153,12 +160,14 @@ def gyro_stabilized():
         time.sleep(DT)
         stabilized_gyro_omega = stabilized_gyro.Get_CalOut_Value()
 
+# generate array values for every possible motor step
 xmotorposition1 = generatesteps(4,0)
 xmotorposition2 = generatesteps(4,2.0943933333)
 xmotorposition3 = generatesteps(4,4.1887866666)
 ymotorposition1 = generatesteps(4,0)
 ymotorposition2 = generatesteps(4,2.0943933333)
 ymotorposition3 = generatesteps(4,4.1887866666)
+# set up I/O for motor control (set PWM outputs)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
@@ -166,19 +175,22 @@ GPIO.setup(12, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(14, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
-p = GPIO.PWM(10, 200)
-q = GPIO.PWM(11, 200)
-r = GPIO.PWM(12, 200)
-s = GPIO.PWM(13, 200)
-t = GPIO.PWM(14, 200)
-u = GPIO.PWM(15, 200)
-p.start(0)
-q.start(0)
-r.start(0)
-s.start(0)
-t.start(0)
-u.start(0)
+# set PWM frequency
+x1 = GPIO.PWM(10, PWM_FREQ)
+x2 = GPIO.PWM(11, PWM_FREQ)
+x3 = GPIO.PWM(12, PWM_FREQ)
+y1 = GPIO.PWM(13, PWM_FREQ)
+y2 = GPIO.PWM(14, PWM_FREQ)
+y3 = GPIO.PWM(15, PWM_FREQ)
+# enable PWM output
+x1.start(0)
+x2.start(0)
+x3.start(0)
+y1.start(0)
+y2.start(0)
+y3.start(0)
 
+# initialize parallel tasks for reading, processing and output
 readGyroRef = Thread(target=gyro_reference, args=())
 readAccRef = Thread(target=accelerometer_reference, args=())
 readGyroStab = Thread(target=gyro_stabilized, args=())
@@ -187,6 +199,7 @@ filterRef = Thread(target=filter_reference, args=())
 filterStab = Thread(target=filter_stabilized, args=())
 motorXThread = Thread(target=motorX, args=())
 motorYThread = Thread(target=motorY, args=())
+# start tasks
 readGyroRef.start()
 readAccRef.start()
 readGyroStab.start()
@@ -195,17 +208,19 @@ filterRef.start()
 filterStab.start()
 motorXThread.start()
 motorYThread.start()
+# wait for calibration to complete - sensors should be held still
 print "Catching up..."
 time.sleep(2)
 print "Calibrating..."
 time.sleep(25)
 print "Get ready!"
 time.sleep(5)
+# work in progess: display info
 while True:
     try:
         print int(output1[0:2])
         print int(output2[0:2])
         time.sleep(DT)
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: # wait for exit
         GPIO.cleanup()
         os.system("sudo killall python")
