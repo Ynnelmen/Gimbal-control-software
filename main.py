4380ad79b1a22037bc188b83a9788549cbf72435
guru2468 <jeremie.reusser@sunrise.ch>
2015-12-26 22:52:41 +0100

    dsf

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ motorposition3 = 0
 
 def motorX(): # controls x-axis
     pidX = PID.PID(P, I, D)
-    pidX.setSampleTime(0.02)
-    pidX.setKp(1.5)
-    pidX.setKi(0.04)
-    pidX.setKd(25)
+    pidX.setSampleTime(1)
+    pidX.setKp(5)
+    pidX.setKi(0)
+    pidX.setKd(0)
     windupfactor = 10
     time.sleep(17)
     pidX.SetPoint = 30
